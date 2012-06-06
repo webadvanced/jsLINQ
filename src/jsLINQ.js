805426bc6ad5364ddb243afb9512c675dd7c1945
Web Advanced <paul.barton@webadvanced.com>
2012-06-06 16:48:18 -0700

    added prototype checks for all extensions

--- src/jsLINQ.js
+++ src/jsLINQ.js
@@ -47,55 +47,60 @@
         };
     }
     
-    a.fn.first = a.fn.single = function( predicate ) {
-        var items = this;
-        if( !predicate ) {
-            return items[0];
-        }
-        return items.where(predicate)[0];
-    };
-    
-    a.fn.count = function( predicate ) {
-        var items = this;
-        if( predicate  === _undefined ) { 
-            return this.length;
-        }
-        return items.where(predicate).length;
-    };
-
-    a.fn.any = function( predicate ) {
-        var items = this;
-        if( predicate === _undefined ) {
-            return this.length > 0;
-        }
-        return ( items.length <= 0 ) ? false : items.where(predicate).length > 0;
-    };
-
-    a.fn.toHash = a.fn.toDictionary = function( key ) {
-        var items = this, l = items.length, i = 0, obj = {};
-        if(key && items[0][key] === _undefined) throw 'key is only valid for arrays of Object';
-        for( i; i < l; i++ ) {
-            if(!key) {
-                obj[i] = items[i];
-            } else {
-                obj[items[i][key]] = items[i];
+    if(a.first === _undefined || a.single === _undefined) {
+        a.fn.first = a.fn.single = function( predicate ) {
+            var items = this;
+            if( !predicate ) {
+                return items[0];
             }
-        }
+            return items.where(predicate)[0];
+        };
+    }
 
-        return obj;
-    };
+    if(a.count === _undefined) {
+        a.fn.count = function( predicate ) {
+            var items = this;
+            if( predicate  === _undefined ) { 
+                return this.length;
+            }
+            return items.where(predicate).length;
+        };
+    }
+    if(a.any === _undefined) {
+        a.fn.any = function( predicate ) {
+            var items = this;
+            if( predicate === _undefined ) {
+                return this.length > 0;
+            }
+            return ( items.length <= 0 ) ? false : items.where(predicate).length > 0;
+        };
+    }
+    if(a.toHash === _undefined || a.toDictionary === _undefined) {
+        a.fn.toHash = a.fn.toDictionary = function( key ) {
+            var items = this, l = items.length, i = 0, obj = {};
+            if(key && items[0][key] === _undefined) throw 'key is only valid for arrays of Object';
+            for( i; i < l; i++ ) {
+                if(!key) {
+                    obj[i] = items[i];
+                } else {
+                    obj[items[i][key]] = items[i];
+                }
+            }
+            return obj;
+        };
+    }
+    if(a.orderBy === _undefined) {
+        a.fn.orderBy = a.fn.order = function( prop ) {
+            var items = this, l = items.length, type, action;
+            type = ( prop === _undefined ) ? getType( items[0] ) : getType( items[0][prop] );
+            if( prop !== _undefined && type !== '[Object]' && items[0][prop] === _undefined ) throw 'cannot use prop with an Array of primitive types (String, Date, Number, Bool)';
+            if( prop === _undefined ) {
+                return ( type === '[String]' ) ? items.sort( sortString ) : items.sort( sortNumber );
+            }
 
-    a.fn.orderBy = a.fn.order = function( prop ) {
-        var items = this, l = items.length, type, action;
-        type = ( prop === _undefined ) ? getType( items[0] ) : getType( items[0][prop] );
-        if( prop !== _undefined && type !== '[Object]' && items[0][prop] === _undefined ) throw 'cannot use prop with an Array of primitive types (String, Date, Number, Bool)';
-        if( prop === _undefined ) {
-            return ( type === '[String]' ) ? items.sort( sortString ) : items.sort( sortNumber );
-        }
-
-        return ( type === '[String]' ) ? items.sort(sortProxy(sortString, prop)) : items.sort(sortProxy(sortNumber, prop));
-    };
-    
+            return ( type === '[String]' ) ? items.sort(sortProxy(sortString, prop)) : items.sort(sortProxy(sortNumber, prop));
+        };
+    }
     sortProxy = function(func, prop) {
         return (function(a, b) {
                     return func(a, b, prop);
